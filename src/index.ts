aa3c8ad3696f52cd79f1a9a5aea745dcc7b24e32
Matthew McEachen <matthew+github@mceachen.org>
2016-10-17 11:46:12 -0700

    s/dist/lib/

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-export function helloWorld() {
-  console.log('hello world')
+import { Student } from './example/Student'
+import { Person } from './example/Person'
+
+export * from './example/person'
+export * from './example/student'
+
+export class Teacher implements Person {
+  private readonly students: Student[] = []
+  constructor(
+    readonly firstName: string,
+    readonly lastName: string
+  ) { } // tslint:disable-line
+
+  addStudent(student: Student): void {
+    this.students.push(student)
+  }
 }
